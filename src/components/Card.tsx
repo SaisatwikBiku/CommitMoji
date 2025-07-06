0d8f34eb31cd7148904299de92f74729020d1dff
prathmesh-ka-github <pkale@albany.edu>
2025-07-05 20:35:11 -0400

    :art: styling changed for card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
+
+import '../styles/card.css'
+
 interface cardProps {
     carditems: any; // Allows the prop 'myProp' to be of any type
 }
 
 const Card: React.FC<cardProps> = ({carditems}) => {
     return (
-        <div className='w-[16rem] h-[20rem] bg-[#383838] my-7 mx-5 p-5 flex items-center justify-center flex-col rounded-md'>
+        <div className='card-container w-[16rem] h-[20rem] my-7 mx-5 p-5 flex items-center justify-center flex-col rounded-md'>
             <div className='text-[6rem] cursor-pointer emoji'>{carditems.emoji}</div>
             <div className='text-center'>
-                <div className='text-[20px] font-bold cursor-pointer '>{carditems.code}</div>
+                <div className='text-[20px] font-bold cursor-pointer card-code'>{carditems.code}</div>
                 <div className='my-2'>{carditems.desc}</div>
             </div>
         </div>
