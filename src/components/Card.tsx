40c9333f344f79c3cbf60d9fbe5b3b5309d5b55f
prathmesh-ka-github <pkale@albany.edu>
2025-07-05 21:15:09 -0400

    :sparkles: feature added - add to clipboard

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,8 +7,14 @@ interface cardProps {
 }
 
 const Card: React.FC<cardProps> = ({carditems}) => {
+
+    function copyText(text) {
+        navigator.clipboard.writeText(text);
+        console.log("text copied to clipboard!")
+    }
+
     return (
-        <div className='card-container w-[16rem] h-[20rem] flex items-center justify-center flex-col rounded-md'>
+        <div onClick={() => copyText(carditems)} className='card-container w-[16rem] h-[20rem] flex items-center justify-center flex-col rounded-md'>
             <div className='text-[6rem] cursor-pointer emoji'>{carditems.emoji}</div>
             <div className='text-center'>
                 <div className='text-[20px] font-bold cursor-pointer card-code'>{carditems.code}</div>
