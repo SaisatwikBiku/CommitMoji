7ecde79bdece60af388563af360f5d8c1ffa5366
abhishek <abhishek.gourishetty@gmail.com>
2025-07-06 14:50:24 -0400

    :art: improved notification popup with blur and toast animation

--- src/components/Notify.tsx
+++ src/components/Notify.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
 
-interface Notifyprops {
-    visibility:number
+interface NotifyProps {
+    visibility: number
+    emoji: string
 }
-// const Card: React.FC<cardProps>
-const Notify:React.FC<Notifyprops> = (visibility) => {
-    console.log("Notify component")
+
+const Notify: React.FC<NotifyProps> = ({ visibility, emoji }) => {
     return (
-        <div style={{ opacity:  visibility.visibility  }} className='w-80 h-80 bg-red-600 absolute top-0 right-0'>Notify</div>
+        <div
+            style={{
+                opacity: visibility,
+                transition: 'opacity 0.5s ease-in-out, transform 0.5s ease-in-out',
+                transform: visibility ? 'translateY(0)' : 'translateY(-20px)',
+                borderRadius: '16px',
+            }}
+            className='fixed top-5 right-5 bg-gray-600 backdrop-blur-md  text-white px-4 py-2 shadow-lg flex items-center'
+        >
+            Copied to clipboard {emoji}
+        </div>
     )
 }
 
